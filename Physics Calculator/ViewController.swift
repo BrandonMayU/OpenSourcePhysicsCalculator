31ac3fedbddfa7c6164207597629245dc5fc1a13
BrandonMayU <brandonmayu@gmail.com>
2017-05-01 12:05:33 -0700

    Initial Commit

---
+++ Physics Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Physics Calculator
+//
+//  Created by Brandon Mayhew on 2017-05-01.
+//  Copyright © 2017 MayU-Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
